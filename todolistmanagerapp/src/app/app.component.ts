f37d2846a6595a5aa02027cc266ac7c16f284aba
Marian Popovici <48692899+PopoviciMarian@users.noreply.github.com>
2021-03-31 17:10:23 +0300

    adding UI. close #1

--- todolistmanagerapp/src/app/app.component.ts
+++ todolistmanagerapp/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Task } from './task';
 import { TasksService } from './task.service';
 
@@ -26,4 +27,36 @@ export class AppComponent implements OnInit{
       }
     );
   }
+
+  public deliteTask(task: Task):void{
+    this.taskService.deleteTask(task.id).subscribe(
+      (response :void)=>{
+        console.log(response);
+        this.getTasks();
+      },
+      (error: HttpErrorResponse) =>{
+        alert(error.message);
+      }
+    );
+  }
+
+  
+  public addTask(task :Task, string: string):void{
+  
+    task.description = string;
+    task.title = "task";
+    
+    console.log(task);
+    
+    this.taskService.addTask(task).subscribe(
+      (response : Task) =>{
+        console.log(response);
+        this.getTasks();
+      },
+      (error: HttpErrorResponse) =>{
+        alert(error.message);
+      }
+    )
+  }  
+ 
 }
